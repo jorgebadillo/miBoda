e725c41ecdbc52d6ad1923f837a8e91a7ecafb04
jorgebadillo <jbadillowork@gmail.com>
2018-01-23 09:37:22 -0600

    Proyecto Inicial

---
+++ miBoda/miBodaPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace miBoda
+{
+    public partial class miBodaPage : ContentPage
+    {
+        public miBodaPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
